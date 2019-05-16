fd1fd950f287aa2b0c4477aad2c71772fe146a36
Aishwarya  Sontakke <47170923+aishwaryasontakke@users.noreply.github.com>
2019-05-15 23:08:58 -0400

    Update sort_improv.py

--- sort_improv.py
+++ sort_improv.py
@@ -1,7 +1,7 @@
 __author__ = 'AISHWARYA SONTAKKE'
 
 """
-Author: AISHWARYA SONTAKKE as4897
+Author: AISHWARYA SONTAKKE
 
 This program compares two different quick sorting algorithms on the
 given two types of data sets.
